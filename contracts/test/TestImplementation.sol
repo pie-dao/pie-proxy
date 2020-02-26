2a62b9b36b880a581893219010f6d26325a7aecd
Mick de Graaf <mick@mickdegraaf.nl>
2020-02-26 18:12:58 +0100

    overriding tests

--- contracts/test/TestImplementation.sol
+++ contracts/test/TestImplementation.sol
@@ -4,6 +4,15 @@ pragma solidity ^0.6.2;
 contract TestImplementation {
 
     string public value;
+    string public value1;
+
+    function setValue1(string calldata _value) external {
+        value1 = _value;
+    }
+
+    function getValue1() public view returns(string memory) {
+        return value1;
+    }
 
     function setValue(string calldata _value) external {
         value = _value;
--- contracts/test/TestOverrides.sol
+++ contracts/test/TestOverrides.sol
@@ -2,10 +2,14 @@ pragma solidity ^0.6.2;
 
 contract TestOverrides {
 
+    string public value;
+    string public value1;
+
     function doesOverride(bytes4 _selector) public view returns (bool) {
         if(
             _selector == this.name.selector ||
-            _selector == this.symbol.selector
+            _selector == this.symbol.selector ||
+            _selector == this.setValue1.selector
         ) {
             return true;
         }
@@ -19,6 +23,10 @@ contract TestOverrides {
 
     function symbol() public view returns (string memory) {
         return "SYMBOL";
+    }
+
+    function setValue1(string memory _value) public {
+        value1 = "OVERWRITTEN";
     }
 
 }
