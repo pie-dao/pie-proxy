e4212016b7e081608b2770665e0d82cdf120d394
Mick de Graaf <mick@mickdegraaf.nl>
2020-03-11 18:58:18 +0100

    additional buidler tasks

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,4 +1,7 @@
-import { BuidlerConfig, usePlugin } from "@nomiclabs/buidler/config";
+import { BuidlerConfig, usePlugin, task } from "@nomiclabs/buidler/config";
+
+import { PProxyPausableFactory } from "./typechain/PProxyPausableFactory";
+import { PProxyFactory } from "./typechain/PProxyFactory";
 
 usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("@nomiclabs/buidler-etherscan");
@@ -39,5 +42,38 @@ const config: ExtendedBuidlerConfig = {
     target: "ethers"
   }
 };
+
+task("deploy-pausable-proxy", "deploys a pausable proxy contract")
+  .setAction(async(taskArgs, { ethers }) => {
+    const signer = (await ethers.signers())[0];
+    const factory = new PProxyPausableFactory(signer);
+    const proxy = await factory.deploy();
+
+    console.log(`Deployed proxy at: ${proxy.address}`);
+});
+
+task("set-proxy-implementation", "sets the implementation contract of the proxy")
+  .addParam("proxy", "address of the proxy contract")
+  .addParam("implementation", "address of the implementation contract")
+  .setAction(async(taskArgs, { ethers }) => {
+    const signer = (await ethers.signers())[0];
+    const proxy = PProxyFactory.connect(taskArgs.proxy, signer);
+    const tx = await proxy.setImplementation(taskArgs.implementation);
+    const receipt = await tx.wait(1);
+
+    console.log(`Implementation set tx: ${receipt.transactionHash}`);
+});
+
+task("set-proxy-owner", "sets the proxy owner")
+  .addParam("proxy", "address of the proxy contract")
+  .addParam("owner", "adderss of the proxy owner")
+  .setAction(async(taskArgs, { ethers }) => {
+    const signer = (await ethers.signers())[0];
+    const proxy = PProxyFactory.connect(taskArgs.proxy, signer);
+    const tx = await proxy.setProxyOwner(taskArgs.owner);
+    const receipt = await tx.wait(1);
+
+    console.log(`Proxy owner set tx: ${receipt.transactionHash}`);
+});
 
 export default config;
